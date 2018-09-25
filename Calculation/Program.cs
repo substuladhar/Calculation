b800aaa81058aaece08debc5326e38de90b47055
Subhechhya Tuladhar <tuladharsuhcya@gmail.com>
2018-09-25 09:44:41 +0545

    implemented and tested add function

---
+++ Calculation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 2;
+            double b = 4;
+            double sum=add(a, b);
+            Console.WriteLine("SUM = {0}", sum);
+            Console.ReadKey();
+        }
+          
+        static double add(double a, double b)
+        {
+            return (a + b);
+        }
+    
+    }
+}
